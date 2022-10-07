fbb15821d7aff986cfe473462edf1af74e874255
hamza.abidi <abidi.hamza84000@gmail.com>
2022-10-07 13:05:20 +0200

    Fix(access control): Add an access control on retrieving account transactions

--- src/main/java/fr/ing/secu/leakybank/dao/AccountsDAO.java
+++ src/main/java/fr/ing/secu/leakybank/dao/AccountsDAO.java
@@ -14,7 +14,7 @@ import fr.ing.secu.leakybank.model.InternalAccount;
 
 /**
  * DAO that manages internal accounts
- * 
+ *
  * @author chouippea
  *
  */
@@ -25,12 +25,12 @@ public class AccountsDAO {
 	private JdbcTemplate jdbcTemplate;
 
 	/** Maps a result set row to a InternalAccount object */
-	private RowMapper<InternalAccount> internalAccountMapper = (rs, rowNum) -> new InternalAccount(rs.getInt("ACCOUNT_NUMBER"), 
+	private RowMapper<InternalAccount> internalAccountMapper = (rs, rowNum) -> new InternalAccount(rs.getInt("ACCOUNT_NUMBER"),
 			new AccountType(rs.getInt("ACCOUNT_TYPE"), rs.getString("LABEL")), rs.getBigDecimal("AVAILABLE_BALANCE"));
 
 	/**
 	 * Return the list of internal accounts owned by the user
-	 * 
+	 *
 	 * @param userLogin
 	 *            login of the user
 	 * @return the list of internal accounts owned by the user
@@ -42,15 +42,40 @@ public class AccountsDAO {
 
 	/**
 	 * Return an internal account from its account number
-	 * 
+	 *
 	 * @param accountNumber
 	 *            account number of the requested account
 	 * @return an internal account from its account number
 	 */
-	public Optional<InternalAccount> findInternalAccountByAccountNumber(final int accountNumber) {
+	public Optional<InternalAccount> findInternalAccountByAccountNumberAndUser(final int accountNumber) {
 		try {
-		return Optional.of(jdbcTemplate.queryForObject("select * from INTERNAL_ACCOUNTS account, INTERNAL_ACCOUNT_TYPES type where account.account_type=type.id and ACCOUNT_NUMBER=" + accountNumber,
-				internalAccountMapper));
+            return jdbcTemplate.query(
+                    "select * from INTERNAL_ACCOUNTS account, INTERNAL_ACCOUNT_TYPES type where account.account_type = type.id and ACCOUNT_NUMBER = ?",
+                    preparedStatement -> {
+                        preparedStatement.setInt(1, accountNumber);
+                    },
+                    internalAccountMapper).stream().findAny();
+		} catch (EmptyResultDataAccessException ex) {
+			return Optional.empty();
+		}
+	}
+
+	/**
+	 * Return an internal account from its account number
+	 *
+	 * @param accountNumber
+	 *            account number of the requested account
+	 * @return an internal account from its account number
+	 */
+	public Optional<InternalAccount> findInternalAccountByAccountNumberAndUser(final int accountNumber, final String userLogin) {
+		try {
+			return jdbcTemplate.query(
+                    "select * from INTERNAL_ACCOUNTS account, INTERNAL_ACCOUNT_TYPES type where account.account_type = type.id and ACCOUNT_NUMBER = ? and account.owner = ?",
+                    preparedStatement -> {
+                        preparedStatement.setInt(1, accountNumber);
+                        preparedStatement.setString(2, userLogin);
+                    },
+                    internalAccountMapper).stream().findAny();
 		} catch (EmptyResultDataAccessException ex) {
 			return Optional.empty();
 		}
