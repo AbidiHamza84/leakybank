d3c636dd76522d17c914507c464342e7a1d8e7eb
hamza.abidi <abidi.hamza84000@gmail.com>
2022-10-07 09:43:27 +0200

    Fix(login): fix SQL injection on login

--- src/main/java/fr/ing/secu/leakybank/dao/UsersDAO.java
+++ src/main/java/fr/ing/secu/leakybank/dao/UsersDAO.java
@@ -16,29 +16,34 @@ public class UsersDAO {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
+
 	/** Maps USERS table result set to User beans*/
-	private RowMapper<User> userRowMapper = (rs, rowNum) -> new User(rs.getString("LOGIN"), rs.getString("FIRST_NAME"), rs.getString("LAST_NAME"), rs.getBoolean("IS_ADMIN"));
+	private final RowMapper<User> userRowMapper = (rs, rowNum) -> new User(rs.getString("LOGIN"), rs.getString("FIRST_NAME"), rs.getString("LAST_NAME"), rs.getBoolean("IS_ADMIN"));
 
 	/**
 	 * Login a user
-	 * 
+	 *
 	 * @return Details on the user if login credentials are valid, else return null
 	 *         an exception
 	 */
 	public Optional<User> login(String login, String password) {
 		try {
-			return Optional.of(jdbcTemplate.queryForObject("select LOGIN, FIRST_NAME, LAST_NAME, IS_ADMIN from users where login='" + login + "' and password='" + password + "'", userRowMapper));
+			return jdbcTemplate.query(
+                    "select LOGIN, FIRST_NAME, LAST_NAME, IS_ADMIN from users where login = ? and password = ?"
+                    , preparedStatement -> {
+						preparedStatement.setString(1, login);
+						preparedStatement.setString(2, password);
+					}, userRowMapper).stream().findFirst();
 		} catch (EmptyResultDataAccessException ex) {
 			return Optional.empty();
 		}
 	}
-	
+
 	/** Return the list of non-admin users */
 	public List<User> findUsers() {
 		return jdbcTemplate.query("select LOGIN, FIRST_NAME, LAST_NAME, IS_ADMIN from users where IS_ADMIN = false", userRowMapper);
 	}
-	
+
 	public void deleteUser(String login) {
 		jdbcTemplate.update("delete from USERS where login='" + login + "'");
 	}
