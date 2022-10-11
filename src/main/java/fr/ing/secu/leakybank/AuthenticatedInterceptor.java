29b33ef9b831a17df99eee5999baabfc7f115104
hamza.abidi <abidi.hamza84000@gmail.com>
2022-10-11 16:52:23 +0200

    feat(spring-version): permit access for css and js sources by anonymous users in order do display correctly the login page

--- src/main/java/fr/ing/secu/leakybank/AuthenticatedInterceptor.java
+++ src/main/java/fr/ing/secu/leakybank/AuthenticatedInterceptor.java
@@ -5,9 +5,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 /**
  * Redirect unauthenticated users to the login page
@@ -16,19 +15,26 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  *
  */
 @Component
-public class AuthenticatedInterceptor extends HandlerInterceptorAdapter {
-	
-	
+public class AuthenticatedInterceptor implements HandlerInterceptor {
 	private final static Logger LOGGER = LoggerFactory.getLogger(AuthenticatedInterceptor.class);
 
-	@Autowired
-	private UserSession userSession;
+	private final UserSession userSession;
 
-	public boolean preHandle(HttpServletRequest request,
-			HttpServletResponse response, Object handler) throws Exception {
-		
+	public AuthenticatedInterceptor(UserSession userSession) {
+		this.userSession = userSession;
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception  {
+
+		if (request.getRequestURI().startsWith("/js") ||
+				request.getRequestURI().startsWith("/css") ||
+				request.getRequestURI().startsWith("/img")) {
+			return true;
+		}
+
 		LOGGER.debug("Url: {}", request.getRequestURI());
-		
+
 		if (!userSession.isAuthenticated()) {
 			// Force redirection to /login if user is not authenticated
 			if (request.getRequestURI().startsWith("/login") || request.getRequestURI().startsWith("/error")) {
@@ -39,7 +45,7 @@ public class AuthenticatedInterceptor extends HandlerInterceptorAdapter {
 				} else {
 					response.sendRedirect("/login");
 				}
-				
+
 				return false;
 			}
 		}
